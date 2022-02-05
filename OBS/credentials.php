757b63fa1c3220b2aa0e9cf20bb1142902c80690
iamkowit <30616703+iamkowit@users.noreply.github.com>
2022-02-05 11:51:40 +0700

    Create credentials.php

---
+++ OBS/credentials.php
@@ -0,0 +1,9 @@
+<?php
+# Content in credentials.php
+$ak = ' ** your ak ** ';
+$sk = ' ** your ak ** ';
+$endpoint = ' ** https://obsendpoint **';
+$bucketName = ' ** obs bucket';
+$objectKey = ' ** obs objectkey** ';
+
+?>
